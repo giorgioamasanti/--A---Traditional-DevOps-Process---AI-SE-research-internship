a10c8b976996f4f01ac645ba2848af5105e8a26d
giorgioamasanti <giorgioamasanti@gmail.com>
2024-08-08 15:56:23 +0100

    changing os path

--- game_state/drop.py
+++ game_state/drop.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import time
 from user_inputs.commands import executeCommands
 
