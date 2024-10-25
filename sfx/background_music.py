0394f191b5e7c792513c71ce2c704b278bbf8b3f
giorgioamasanti <giorgioamasanti@gmail.com>
2024-10-25 18:09:54 +0100

    trying the resource_path thing

--- sfx/background_music.py
+++ sfx/background_music.py
@@ -4,9 +4,19 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pygame
 
+def resource_path(relative_path):
+    """ Get the absolute path to a resource, works for both dev and PyInstaller. """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 class backgroundMusic():
     def __init__(self):
-        self.file = "assets/music_and_sounds/background_music.mp3"
+        self.file = resource_path("assets/music_and_sounds/background_music.mp3")
         self.background_music_playing = True
         pygame.mixer.music.load(self.file)
         pygame.mixer.music.play()
