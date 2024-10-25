7d96c368b63d5fb0f0d8a9a2de41763c90817dee
giorgioamasanti <giorgioamasanti@gmail.com>
2024-10-25 18:26:37 +0100

    resource path to all sfx

--- sfx/gameplay_sfx.py
+++ sfx/gameplay_sfx.py
@@ -4,15 +4,24 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pygame
 
+def resource_path(relative_path):
+    """ Get the absolute path to a resource, works for both dev and PyInstaller. """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 pygame.mixer.init()
 
 #dictionary which maps name of sound effect to the path to that file
-sound_library = {"game_over": "assets/music_and_sounds/game_over.mp3",
-                 "move_piece": "assets/music_and_sounds/move_piece.mp3",
-                 "rotate_piece": "assets/music_and_sounds/rotate_piece.mp3",
-                 "line_clear": "assets/music_and_sounds/line_clear.mp3",
-                 "solidify": "assets/music_and_sounds/solidify.mp3"}
+sound_library = {"game_over": resource_path("assets/music_and_sounds/game_over.mp3"),
+                 "move_piece": resource_path("assets/music_and_sounds/move_piece.mp3"),
+                 "rotate_piece": resource_path("assets/music_and_sounds/rotate_piece.mp3"),
+                 "line_clear": resource_path("assets/music_and_sounds/line_clear.mp3"),
+                 "solidify": resource_path("assets/music_and_sounds/solidify.mp3")}
 
 def sfx(sound):
 
